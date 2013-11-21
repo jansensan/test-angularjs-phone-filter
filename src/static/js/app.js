9ef9e7b87441404ce9a0ca8d221b3479a53d274a
Mat Janson Blanchet <mat.jb@jansensan.net>
2013-11-21 14:47:49 -0500

    Implemented angular filter.

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,6 +1,28 @@
 angular.module('page', [])
 
-.controller('pageController', function () {
+.filter('northAmericanPhoneNumber', function () {
+  return function (input) {
+    var output = '';
 
+    var numbers = input.match(/(\d)/g);
+    if (numbers) {
+      var i = 0;
+      var numNumbers = numbers.length;
+      if (numNumbers > 10) {
+        numNumbers = 10;
+      }
+      for (i; i < numNumbers; i++) {
+        if (i === 3 || i === 6) {
+          output += '-';
+        }
+        output += numbers[i];
+      }
+    }
 
+    return output;
+  };
+})
+
+.controller('pageController', function ($scope) {
+  $scope.phone = '';
 })
